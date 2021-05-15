20cef868adcc86f158ac9cfc27a1505527b4b030
Sergey Pushkovskiy <spushkovskiy@gmail.com>
2021-05-15 07:57:48 +0300

    finished users

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,38 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+const User = require('./user.model');
+
+const user1 = new User({
+  name: 'John',
+  login: 'johny1',
+  password: 'youknownothing',
+});
+const user2 = new User({
+  name: 'Aria',
+  login: 'astark1',
+  password: 'valarmorgulis',
+});
+const user3 = new User({
+  name: 'Hodor',
+  login: 'howdy',
+  password: 'holdthedoor',
+});
+const user4 = new User({
+  name: 'Daeneris',
+  login: 'khalise',
+  password: 'motherofdragons',
+  id: '1',
+});
+
+const users = [user1, user2, user3, user4];
+
+const getAll = async () => users;
+const getById = async (id) => users.find((user) => user.id === id);
+const createUser = async (user) => users.push(user);
+
+const updateUser = async (user, body) => {
+  const idx = users.findIndex((item) => item.id === user.id);
+  users[idx] = { ...user, ...body };
 };
 
-module.exports = { getAll };
+const deleteUser = async (id) => users.filter((user) => user.id !== id);
+
+module.exports = { getAll, getById, createUser, deleteUser, updateUser };
