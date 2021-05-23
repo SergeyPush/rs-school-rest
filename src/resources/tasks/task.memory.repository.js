5a5842e42e346c082b16f0c33dd804959e25b493
Sergey Pushkovskiy <spushkovskiy@gmail.com>
2021-05-23 17:42:44 +0300

    added jsdoc descriptions to tasks

--- src/resources/tasks/task.memory.repository.js
+++ src/resources/tasks/task.memory.repository.js
@@ -26,34 +26,58 @@ const task3 = new Task({
   id: '1',
 });
 let tasks = [task1, task2, task3];
-
+/**
+ * @param  {string} boardId Id of a board
+ * @returns {array} Return array of all tasks of particular board
+ */
 const getAll = async (boardId) =>
   tasks.filter((task) => task.boardId === boardId);
 
+/**
+ * @param  {string} boardId Id of a board
+ * @param  {string} taskId Id of a task
+ * @returns {object} Returns single task object
+ */
 const getById = async (boardId, taskId) =>
   tasks
     .filter((task) => task.boardId === boardId)
     .find((task) => task.id === taskId);
-
+/**
+ * @param  {object} task Task that is going to be added
+ */
 const createTask = async (task) => tasks.push(task);
 
+/**
+ * @param  {string} boardId Id of a board
+ * @param  {string} taskId Id of a task to be deleted
+ * @returns {array} List of all tasks
+ */
 const deleteTask = async (boardId, taskId) => {
   tasks = tasks.filter((task) => task.id !== taskId);
   return tasks;
 };
-
+/**
+ * @param  {object} task Task that is going to be changed
+ * @param  {object} body Object with data that should be updated
+ * @returns {object} Returns changed task
+ */
 const updateTask = async (task, body) => {
   const idx = tasks.findIndex((t) => t.id === task.id);
   tasks[idx] = { ...task, ...body };
   return tasks[idx];
 };
 
+/**
+ * @param  {string} userId Id of user that is deleted
+ */
 const deleteUser = async (userId) => {
   tasks = tasks.map((task) =>
     task.userId === userId ? { ...task, userId: null } : { ...task }
   );
 };
-
+/**
+ * @param  {string} boardId Id of board that is deleted
+ */
 const deleteBoard = async (boardId) => {
   tasks = tasks.filter((task) => task.boardId !== boardId);
 };
--- src/resources/tasks/task.service.js
+++ src/resources/tasks/task.service.js
@@ -1,14 +1,38 @@
 const taskRepo = require('./task.memory.repository');
-
+/**
+ * @param  {string} boardId Id of requested board
+ * @returns {array} List of all boards
+ */
 const getAll = (boardId) => taskRepo.getAll(boardId);
+
+/**
+ * @param  {string} boardId Id of requested Board
+ * @param  {string} taskId Id of requested Task
+ * @returns  {object} Returns objet of Task
+ */
 const getOne = (boardId, taskId) => taskRepo.getById(boardId, taskId);
 
+/**
+ * @param  {object} task Task object
+ * @returns {object} Object of created Task
+ */
 const createOne = (task) => {
   taskRepo.createTask(task);
   return getOne(task.boardId, task.id);
 };
+
+/**
+ * @param  {string} boardId Id of Board
+ * @param  {string} taskId Id of Task
+ * @returns {array} list of all Tasks of requested Board
+ */
 const deleteOne = (boardId, taskId) => taskRepo.deleteTask(boardId, taskId);
 
+/**
+ * @param  {string} task Id of Task to update
+ * @param  {object} body Object with data to update task
+ * @returns {object} Returns updated task
+ */
 const updateOne = (task, body) => taskRepo.updateTask(task, body);
 
 module.exports = { getAll, getOne, createOne, deleteOne, updateOne };
