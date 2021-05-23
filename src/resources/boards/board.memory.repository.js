ff0ef7812da41777f7c558919a834d795fd5945b
Sergey Pushkovskiy <spushkovskiy@gmail.com>
2021-05-23 17:41:36 +0300

    added jsdoc descriptions to boards

--- src/resources/boards/board.memory.repository.js
+++ src/resources/boards/board.memory.repository.js
@@ -6,15 +6,36 @@ const board3 = new Board({ title: 'New board3' });
 const board4 = new Board({ title: 'New board4', id: '1' });
 
 let boards = [board1, board2, board3, board4];
-
+/**
+ * @returns {array} list of all existing boards
+ */
 const getAll = async () => boards;
+
+/**
+ * @param  {string} id id of searched board
+ * @returns {object} object with the searched board
+ */
 const getById = async (id) => boards.find((board) => board.id === id);
+
+/**
+ * @param  {object} board Accepts board object
+ */
 const createBoard = async (board) => boards.push(board);
+
+/**
+ * @param  {string} id Id of Board to be deleted
+ * @returns {array} Returns list of all boards
+ */
 const deleteBoard = async (id) => {
   boards = boards.filter((board) => board.id !== id);
   return boards;
 };
 
+/**
+ * @param  {object} board Board that should be changed
+ * @param  {object} body Data that chould be updated in board
+ * @returns {object} Returns board that is changed
+ */
 const updateBoard = async (board, body) => {
   const idx = boards.findIndex((b) => b.id === board.id);
   boards[idx] = { ...board, ...body };
--- src/resources/boards/board.service.js
+++ src/resources/boards/board.service.js
@@ -1,12 +1,30 @@
 const boardRepo = require('./board.memory.repository');
 const taskRepo = require('../tasks/task.memory.repository');
 
+/**
+ * @returns {array} Returns array of boards
+ */
 const getAll = () => boardRepo.getAll();
+
+/**
+ * @param  {string} id Of requested board
+ * @returns  {object} Object
+ */
 const getOne = (id) => boardRepo.getById(id);
+
+/**
+ * @param  {object} board Board object
+ * @returns {object} Created board
+ */
 const createOne = (board) => {
   boardRepo.createBoard(board);
   return getOne(board.id);
 };
+
+/**
+ * @param  {string} id
+ * @returns {object} Returns object of deleted board
+ */
 const deleteOne = async (id) => {
   await taskRepo.deleteBoard(id);
   return boardRepo.deleteBoard(id);
