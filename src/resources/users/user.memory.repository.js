5e1c67ead152c1534cfc0e6c7f34db5374e2747f
Sergey Pushkovskiy <spushkovskiy@gmail.com>
2021-05-23 18:09:42 +0300

    added jsdoc to users

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -23,16 +23,36 @@ const user4 = new User({
 });
 
 const users = [user1, user2, user3, user4];
-
+/**
+ * @returns {array} List of all users
+ */
 const getAll = async () => users;
+
+/**
+ * @param  {string} id Id of a user
+ * @returns {object} Object with a particular user
+ */
 const getById = async (id) => users.find((user) => user.id === id);
+
+/**
+ * @param  {object} user Takes User object
+ * @returns {undefined} Function adds user to users list
+ */
 const createUser = async (user) => users.push(user);
 
+/**
+ * @param  {object} user User object
+ * @param  {object} body Data that should be updated in user object
+ */
 const updateUser = async (user, body) => {
   const idx = users.findIndex((item) => item.id === user.id);
   users[idx] = { ...user, ...body };
 };
 
+/**
+ * @param  {string} id Id of user to be deleted
+ * @returns {object} Returns deleted user
+ */
 const deleteUser = async (id) => users.filter((user) => user.id !== id);
 
 module.exports = { getAll, getById, createUser, deleteUser, updateUser };
